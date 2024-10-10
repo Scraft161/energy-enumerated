e001cb4389fbcd573c364768bf96de58c7170d5f
Scraft161 <Scraft161@gmail.com>
2024-10-10 21:12:51 +0200

    and reflect that in main.js

--- target/main.js
+++ target/main.js
@@ -1,6 +1,8 @@
 "use strict";
 //import { incrementEnergy } from './resources.js';
 //import { newSave, loadSave, writeSave } from './save.js';
+// precalculated list of number sequences.
+const fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765];
 var data;
 let data_raw = localStorage.getItem("data");
 // Either retrieve data from localStrorage, or fall back to default.
